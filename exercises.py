e6fc4e1cf5093e3d3e2f43b0224e25d70fdb4830
SayedAli <sayed.ali.majed@gmail.com>
2025-11-28 23:56:12 +0300

    Functions Lab completed

--- exercises.py
+++ exercises.py
@@ -138,3 +138,54 @@ def calculate_tip (bill,percentage ):
 
 print('Exercise 7:', calculate_tip(50, 13))
 print("----------------------------------------------------------------")
+
+# Exercise 8: Calculate Product of Numbers
+#
+# Write a function named `product` that takes an arbitrary number of numbers, multiplies them, and returns the product.
+# Review your notes on *args for handling an arbitrary number of arguments.
+#
+# Examples:
+# product(-1, 4) should return -4.
+# product(2, 5, 5) should return 50.
+#
+# Define the function and call it with different sets of numbers to test.
+
+def product(*args):
+    result =1
+    for num in args:
+        result *= num
+    return result
+
+
+print('Exercise 8:', product(2, 5, 5))
+print("----------------------------------------------------------------")
+
+# Exercise 9: Basic Calculator
+#
+# Create a function named `basic_calculator` that takes three arguments: 
+# two numbers and a string representing an operation ('add', 'subtract', 'multiply', 'divide'). 
+# Perform the provided operation on the two numbers. In operations where the order of numbers is important, 
+# treat the first parameter as the first operand and the second parameter as the second operand.
+#
+# Examples:
+# basic_calculator(10, 5, 'subtract') should return 5.
+# basic_calculator(10, 5, 'add') should return 15.
+# basic_calculator(10, 5, 'multiply') should return 50.
+# basic_calculator(10, 5, 'divide') should return 2.
+#
+# Define the function and then call it below.
+
+def basic_calculator (num, num1,operand):
+    if operand == "add":
+        return num + num1
+    elif operand == "subtract":
+        return num - num1
+    elif operand == "multiply":
+        return num * num1
+    else:
+        operand == "divide"
+        return num//num1
+
+
+
+print('Exercise 9 Result:', basic_calculator(10, 5, "divide"))
